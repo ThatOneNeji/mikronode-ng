43d5146c34df555edb23ef8cfd023ec51b18bc4e
ThatOneNeji <systemlorddice@gmail.com>
2026-07-18 17:34:59 +0200

    Fixed linting issues

--- examples/getInterfacesAndRoutes.js
+++ examples/getInterfacesAndRoutes.js
@@ -1,56 +1,56 @@
 // src interface
 /* jshint undef: true */
 /* globals Promise */
-var MikroNode = require('../lib/index.js');
+const MikroNode = require('../lib/index.js');
 
 /* Don't be surprised if you see the output interleaved. :) */
 
-var c1 = MikroNode.getConnection(process.argv[2], process.argv[3], process.argv[4]);
+const c1 = MikroNode.getConnection(process.argv[2], process.argv[3], process.argv[4]);
 c1.closeOnDone = true;
-c1.connect(function(c) {
-	var o = c.openChannel();
-	o.closeOnDone = true;
-	o.write('/interface/print', function(channel) {
-		console.log('Getting Interfaces');
-		channel.once('done', function(p, chan) {
-			var d = MikroNode.parseItems(p);
-			d.forEach(function(i) {
-				console.log(JSON.stringify(i));
-			});
-		});
-		channel.on('trap', function(trap, chan) {
-			console.log('Command failed: ' + trap);
-		});
-		channel.on('error', function(err, chan) {
-			console.log('Oops: ' + err);
-		});
-	});
-	o.write('/ip/route/print', function(channel) {
-		console.log('Getting routes');
-		channel.on('done', function(p, chan) {
-			console.log('Routes:');
-			var d = MikroNode.parseItems(p);
-			d.forEach(function(i) {
-				console.log(JSON.stringify(i));
-			});
-		});
-	});
+c1.connect(function (c) {
+    const o = c.openChannel();
+    o.closeOnDone = true;
+    o.write('/interface/print', function (channel) {
+        console.log('Getting Interfaces');
+        channel.once('done', function (p, chan) {
+            const d = MikroNode.parseItems(p);
+            d.forEach(function (i) {
+                console.log(JSON.stringify(i));
+            });
+        });
+        channel.on('trap', function (trap, chan) {
+            console.log('Command failed: ' + trap);
+        });
+        channel.on('error', function (err, chan) {
+            console.log('Oops: ' + err);
+        });
+    });
+    o.write('/ip/route/print', function (channel) {
+        console.log('Getting routes');
+        channel.on('done', function (p, chan) {
+            console.log('Routes:');
+            const d = MikroNode.parseItems(p);
+            d.forEach(function (i) {
+                console.log(JSON.stringify(i));
+            });
+        });
+    });
 });
 
 /* Now let's do this with Promises */
 
-var connection = MikroNode.getConnection(process.argv[2], process.argv[3], process.argv[4], {
-	closeOnDone : false
+const connection = MikroNode.getConnection(process.argv[2], process.argv[3], process.argv[4], {
+    closeOnDone: false
 });
 
-var connPromise = connection.getConnectPromise().then(function(conn) {
-	var chan1Promise = conn.getCommandPromise('/interface/print');
-	var chan2Promise = conn.getCommandPromise('/ip/route/print');
-	Promise.all([ chan1Promise, chan2Promise ]).then(function resolved(values) {
-		console.log('Interfaces via Promise: ' + JSON.stringify(values[0]) + '\n\n');
-		console.log('Routes via Promise: ' + JSON.stringify(values[1]) + '\n\n');
-		conn.close();
-	}, function rejected(reason) {
-		console.log('Oops: ' + reason);
-	});
+const connPromise = connection.getConnectPromise().then(function (conn) {
+    const chan1Promise = conn.getCommandPromise('/interface/print');
+    const chan2Promise = conn.getCommandPromise('/ip/route/print');
+    Promise.all([chan1Promise, chan2Promise]).then(function resolved(values) {
+        console.log('Interfaces via Promise: ' + JSON.stringify(values[0]) + '\n\n');
+        console.log('Routes via Promise: ' + JSON.stringify(values[1]) + '\n\n');
+        conn.close();
+    }, function rejected(reason) {
+        console.log('Oops: ' + reason);
+    });
 });
